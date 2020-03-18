ca31a2b5f89bb95537c8547074950f079e0ef060
ahuiali <15900135325@163.com>
2020-03-19 00:15:12 +0800

    更新alibaba的fastjson版本 修改了初始拦截器配置类的名字

--- src/main/java/com/ahuiali/word/interceptor/InitInterceptorConfig.java
+++ src/main/java/com/ahuiali/word/interceptor/InitInterceptorConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class IntercepterConfig  implements WebMvcConfigurer {
+public class InitInterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor())
